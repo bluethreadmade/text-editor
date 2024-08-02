c9bfa8d0caf9489039dbfd644efec6b4b142d1ed
Megan McNeill <mcneim2@gmail.com>
2024-08-02 15:22:31 -0400

    corrected sw

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,21 +17,24 @@ export const putDb = async (content) => {
   console.log('Adding content to the database:', content);
 
   // Open the IndexedDB database named "contact" with version 1
-  const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('jate', 1);
 
   // Create a read-write transaction on the "contact" object store
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = contactDb.transaction('jate', 'readwrite');
 
   // Access the "contact" object store within the transaction
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Add the content to the object store
-  await store.add(content);
+  // await store.add(content);
+  const request = await store.put({ id: 1, value: content });
+
+  console.log(request.value);
 
   console.log('Content added to the database');
 
   // Ensure the transaction is completed
-  await tx.done;
+  // await tx.done;
 }
 // console.error('putDb not implemented');
 
@@ -40,21 +43,21 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contentDb = await openDB('content', 1);
+  const contentDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contentDb.transaction('content', 'readonly');
+  const tx = contentDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('content');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
